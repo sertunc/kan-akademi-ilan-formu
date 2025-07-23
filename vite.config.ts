316ee79d47bb22c30e8ee7e242ed9e1a31f16b9c
Sertunc <sertuncselen@yahoo.com>
2025-07-23 11:42:50 +0300

    chore: update package.json and yarn.lock for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  const config = {
+    plugins: [react()],
+    base: '/kan-akademi-ilan-formu',
+  }
+
+  return config
 })
