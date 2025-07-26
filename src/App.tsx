a6af0f980fc820abc416ab59e867772f188207db
Sertunc SELEN <sertuncselen@yahoo.com>
2025-07-26 15:15:26 +0300

    integrate SweetAlert2 for modal and user notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 import PageHeader from "./components/PageHeader";
 import BloodDonationForm from "./components/BloodDonationForm";
 import LegalNotice from "./components/LegalNotice";
 
 function App() {
+  useEffect(() => {
+    Swal.fire({
+      //title: "Bilgilendirme",
+      text: "Geçmiş olsun dileklerimizi iletiyoruz. Umarız en kısa sürede uygun kan bulunur ve sağlığınıza kavuşursunuz. Yanınızdayız!",
+      //icon: "info",
+      confirmButtonText: "Kapat",
+    });
+  }, []);
+
   return (
     <>
       <PageHeader />
