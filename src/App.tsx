8fbbb2ceea6b36e0bf66b948089ee753bbd89bf3
Sertunc SELEN <sertuncselen@yahoo.com>
2025-07-26 14:53:53 +0300

    add LegalNotice component to display privacy information

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import PageHeader from "./components/PageHeader";
 import BloodDonationForm from "./components/BloodDonationForm";
+import LegalNotice from "./components/LegalNotice";
 
 function App() {
   return (
     <>
       <PageHeader />
       <BloodDonationForm />
+      <LegalNotice />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LegalNotice.tsx
@@ -0,0 +1,20 @@
+function LegalNotice() {
+  return (
+    <div
+      style={{
+        marginTop: "1rem",
+        textAlign: "center",
+        fontSize: "0.95rem",
+        color: "#888",
+      }}
+    >
+      <p>
+        Girilen bilgiler hiçbir şekilde sunucuda veya başka bir ortamda
+        saklanmaz. Formu doldurduktan sonra veriler yalnızca görsel üzerinde
+        gösterilir ve gizliliğiniz korunur.
+      </p>
+    </div>
+  );
+}
+
+export default LegalNotice;
