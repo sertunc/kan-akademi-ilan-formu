1ad245fdccd3935f9a0778b58779a1834787a505
Sertunc SELEN <sertuncselen@yahoo.com>
2025-07-31 00:01:05 +0300

    Add Logo component to display Kan Akademi logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import Swal from "sweetalert2";
+import Logo from "./components/Logo";
 import PageHeader from "./components/PageHeader";
 import BloodDonationForm from "./components/BloodDonationForm";
 import LegalNotice from "./components/LegalNotice";
@@ -7,14 +7,7 @@ import { useTranslation } from "react-i18next";
 import "./i18n";
 
 function App() {
-  const { t, i18n } = useTranslation();
-
-  // useEffect(() => {
-  //   Swal.fire({
-  //     text: t("greeting"),
-  //     confirmButtonText: t("close"),
-  //   });
-  // }, [t]);
+  const { i18n } = useTranslation();
 
   useEffect(() => {
     const userLanguage = navigator.language;
@@ -25,6 +18,7 @@ function App() {
     <>
       <button onClick={() => i18n.changeLanguage("en")}>EN</button>
       <button onClick={() => i18n.changeLanguage("tr")}>TR</button>
+      <Logo />
       <PageHeader />
       <BloodDonationForm />
       <LegalNotice />
---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => (
+  <img
+    src="kan-akademi-logo.png"
+    alt="Kan Akademi Logo"
+    style={{
+      width: "30px",
+      margin: "0 auto",
+      display: "block",
+    }}
+  />
+);
+
+export default Logo;
